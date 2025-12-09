a06f119376ec363d72a13ab998f2797ad4c9d89f
priscilaalves8-design <priscila.alves8@gmail.com>
2025-12-09 15:08:19 -0300

    vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
-// Configuração do Vite apenas para o front (pasta client)
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
     },
   },
 })
